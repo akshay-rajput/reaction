f86dff9e5822cca13a7e3303eabff8e15c15bc37
Akshay <akshayrajput102@gmail.com>
2021-09-12 21:42:53 +0530

    feat - features - add sample child components

---
+++ src/features/About/AboutContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AboutContent() {
+    return (
+        <div className="text-center text-gray-300">
+            <h3 className="text-3xl text-gray-500 font-semibold mb-2">About page</h3>
+            <p className="text-md font-medium">This is just for demonstrate how you'll add new routes to the app and have components inside those pages.</p>
+        </div>
+    )
+}
---
+++ src/features/Home/HomepageContent.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function HomepageContent() {
+    return (
+        <div className="template-intro">
+            <h2 className="text-4xl font-semibold text-gray-500 mb-4">
+                Reaction
+            </h2>
+            <p className="text-lg text-center font-medium mb-2">
+                A quick starter template for react.
+            </p>
+            <p className="text-md text-center font-medium mb-2">
+                Includes{" "}
+                <a
+                    href="https://tailwindcss.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    tailwindCSS
+                </a>
+                , &nbsp;
+                <a
+                    href="https://github.com/remix-run/react-router/blob/dev/docs/advanced-guides/migrating-5-to-6.md"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    react-router-v6
+                </a>
+                , &nbsp;
+                <a
+                    href="https://redux-toolkit.js.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    redux-toolkit
+                </a>
+                &nbsp;and uses&nbsp;
+                <a
+                    href="https://vitejs.dev/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    vite
+                </a>{" "}
+                as a build tool.
+            </p>
+        </div>
+    );
+}
