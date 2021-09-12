1edb60ee6c19f54356db2a66d17eb88540d00f9b
Akshay <akshayrajput102@gmail.com>
2021-09-12 22:32:19 +0530

    feat - App - render routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,44 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import "tailwindcss/tailwind.css";
+import "./App.css";
+
+// redux
+import { useDispatch } from 'react-redux';
+import { setupCounter } from "./features/home/homeSlice";
+
+// components
+import TheNavbar from "./features/shared/TheNavbar";
+import TheFooter from "./features/shared/TheFooter";
+import Home from "./screens/Home";
+import About from "./screens/About";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const dispatch = useDispatch();
+    const initialCount = 10;
+    const dispatch = useDispatch();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    useEffect(() => {
+      // dispatch initial action
+      dispatch(setupCounter(initialCount));
+    }, []);
+
+    return (
+        <div className="App">
+            <TheNavbar />
+            
+            <main className="container mx-auto">
+              {/* render routes inside main container */}
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about/*" element={<About />} />
+              </Routes>
+            </main>
+            
+            <TheFooter />
+        </div>
+    );
 }
 
-export default App
+export default App;
