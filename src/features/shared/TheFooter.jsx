9e5996f76f1e0c128f3103ba5442f3afd02d2ad8
Akshay <akshayrajput102@gmail.com>
2021-09-12 20:02:14 +0530

    feat - Footer - add a sample footer

---
+++ src/features/shared/TheFooter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TheFooter() {
+    return (
+        <footer className="bg-gray-800 text-gray-300 p-2 text-center w-full">
+            Made by <a href="https://github.com/akshay-rajput" target="_blank" className="text-yellow-500 hover:text-yellow-300">Akshay</a>.
+        </footer>
+    )
+}
