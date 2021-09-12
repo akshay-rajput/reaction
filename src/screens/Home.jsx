008204c09df89c78e4e5569d1e66e0f515ef3264
Akshay <akshayrajput102@gmail.com>
2021-09-12 22:31:43 +0530

    feat - homepage - render homepage content

---
+++ src/screens/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomepageContent from "../features/Home/HomepageContent";
+
+export default function Home() {
+    return (
+        <>
+            <HomepageContent />   
+        </>
+    )
+}
