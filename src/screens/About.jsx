123020eb5379d34a6cfc0fb3be766cdbe6c1dfe6
Akshay <akshayrajput102@gmail.com>
2021-09-12 21:44:15 +0530

    feat - About - add about page

---
+++ src/screens/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AboutContent from '../features/About/AboutContent';
+
+export default function About() {
+    return (
+        <div className="template-intro">
+          <AboutContent />  
+        </div>
+    )
+}
