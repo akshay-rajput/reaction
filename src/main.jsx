4aa992305b8739e9cb81121578fb664daaacbe09
Akshay <akshayrajput102@gmail.com>
2021-09-12 22:28:33 +0530

    feat - Main - wrap app inside router & store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import {store} from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+              <App />
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
