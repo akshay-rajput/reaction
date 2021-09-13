c263a0a3fb90fe1e8e5eaefc94ac9dc7620d0128
Akshay <akshayrajput102@gmail.com>
2021-09-13 15:37:16 +0530

    chore - about content - fix grammar

--- src/features/About/AboutContent.jsx
+++ src/features/About/AboutContent.jsx
@@ -4,7 +4,7 @@ export default function AboutContent() {
     return (
         <div className="text-center text-gray-300">
             <h3 className="text-3xl text-gray-500 font-semibold mb-2">About page</h3>
-            <p className="text-md font-medium">This is just for demonstrate how you'll add new routes to the app and have components inside those pages.</p>
+            <p className="text-md font-medium">This is just to demonstrate how you'll add new routes to the app and have components inside those pages.</p>
         </div>
     )
 }
